367cf3efec3dd68a386a6412ef848579c31a0534
ameerhamza91 <ameerhamza@rebeltechnology.io>
2021-03-24 10:48:13 +0500

    axios package installed and header,button components are made

---
+++ src/components/core/button/Button.js
@@ -0,0 +1,6 @@
+import "./button.css";
+
+function Button() {
+  return <button>Go</button>;
+}
+export default Button;
---
+++ src/components/core/header/Header.js
@@ -0,0 +1,10 @@
+import "./header.css";
+
+function Header() {
+  return (
+    <header id="header">
+      <h1>Test App</h1>
+    </header>
+  );
+}
+export default Header;
